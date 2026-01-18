76ef5d2039e38671b5075c9c4c347ac3cfcab795
Oleg Savelyev <oleg.savelev@gmail.com>
2026-01-17 21:21:24 -0800

    remove prongs from travel adapters box model

--- src/sava/csg/build123d/models/other/poweradapters.py
+++ src/sava/csg/build123d/models/other/poweradapters.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from math import tan, radians
 from typing import Tuple
 
-from build123d import Vector, mirror, Plane, Solid, Axis, Location, Face, Polyline, extrude
+from build123d import Vector, mirror, Plane, Axis, Location, Face, Polyline, extrude
 
 from sava.csg.build123d.common.exporter import export, save_3mf, clear, save_stl
 from sava.csg.build123d.common.geometry import create_vector, Alignment
@@ -25,17 +25,7 @@ class RecessDimensions:
     bottom_length: float = 24.5
     bottom_length_flat: float = 15.6
 
-    depth: float = 6
-
-class ProngDimensions:
-    top_diameter: float = 5.1
-    distance_between_top_centres: float = 19.0
-    distance_between_top_and_bottom_centres_y: float = 20.2
-
-    bottom_diameter: float = 7.1
-    bottom_shift_y: float = 4.6
-
-    height: float = 10
+    depth: float = 7
 
 
 class LockDimensions:
@@ -61,7 +51,6 @@ class LockDimensions:
 @dataclass
 class PowerAdapterBoxDimensions:
     recess: RecessDimensions = field(default_factory=RecessDimensions)
-    prongs: ProngDimensions = field(default_factory=ProngDimensions)
     lock: LockDimensions = field(default_factory=LockDimensions)
 
     text: TextDimensions = field(default_factory=lambda: TextDimensions(font_size=24, font="Liberation Sans", height=0.8))
@@ -264,7 +253,7 @@ class PowerAdapterLid(PowerAdapterBase):
 
 class PowerAdapterBox(PowerAdapterBase):
     # Assembles complete box with socket recesses and lock slots
-    def create_box(self) -> Tuple[SmartSolid, SmartSolid]:
+    def create_box(self) -> SmartSolid:
         box_top = create_tapered_box_delta(self.dim.lid_length_internal, self.dim.lid_width_internal, self.dim.lid_cutout_height, -self.dim.box_taper_diff, self.dim.box_fillet_radius)
 
         box_protrusion = self.create_protrusion(True)
@@ -275,25 +264,18 @@ class PowerAdapterBox(PowerAdapterBase):
 
         box = SmartSolid(box_top, box_bottom, box_protrusion, label="box")
 
-        recess_row = self.create_row(self.create_socket_recess()).align_z(box, Alignment.RL)
-        prongs_row = self.create_row(self.create_prongs()).align_z(recess_row, Alignment.LR)
+        element = self.create_socket_recess()
 
-        prongs = []
-
-        for orientation, alignment in [[0, Alignment.LR], [180, Alignment.RL]]:
-            recess_row.orient((0, 0, orientation))
-            recess_row.align_xy(box).move_vector(create_vector(recess_row.y_size / 2 + self.dim.socket_padding / 2, 180 - orientation))
-
-            prongs_row.orient((0, 0, orientation))
-            prongs_row.align_x(box).align_y(recess_row, alignment, alignment.shift_towards_centre(self.dim.prongs.bottom_shift_y))
-            box.cut(recess_row.copy())
-            prongs.append(prongs_row.copy())
+        recess_row = element.clone(self.dim.sockets_per_row, Vector(self.dim.socket_side + self.dim.socket_padding, 0))
+        recess_row.fuse(recess_row.mirrored().align_y(recess_row, Alignment.RR, self.dim.socket_padding))
+        recess_row.align_xy(box).align_z(box, Alignment.RL)
+        box.cut(recess_row)
 
         slot = self.create_lock_slot()
         for orientation, alignment in [[180, Alignment.LR], [0, Alignment.RL]]:
             slot.orient((0, 0, orientation)).align_x(box, alignment).align_y(box).align_z(box, Alignment.LR)
             box.cut(slot)
-        return box, SmartSolid(prongs, label="prongs")
+        return box
 
     # Creates shaped recess for one power adapter socket
     def create_socket_recess(self):
@@ -305,24 +287,6 @@ class PowerAdapterBox(PowerAdapterBase):
         pencil.left(pencil.location.X)
         solid = pencil.extrude(self.dim.recess.depth)
         return SmartSolid(solid, mirror(solid, Plane.YZ))
-
-    # Creates 3-prong pattern for visualizing adapter fit
-    def create_prongs(self) -> SmartSolid:
-        top_left_prong = self.create_prong(self.dim.prongs.top_diameter)
-        top_right_prong = self.create_prong(self.dim.prongs.top_diameter).move(self.dim.prongs.distance_between_top_centres)
-        top_prongs = SmartSolid(top_left_prong, top_right_prong)
-        bottom_prong = self.create_prong(self.dim.prongs.bottom_diameter)
-
-        bottom_prong.align_x(top_prongs).align_z(top_prongs, Alignment.LR).align_y(top_prongs, Alignment.C, -self.dim.prongs.distance_between_top_and_bottom_centres_y)
-
-        return SmartSolid(top_prongs, bottom_prong)
-
-    # Creates single cylindrical prong with hemisphere tip
-    def create_prong(self, diameter: float) -> SmartSolid:
-        cylinder = SmartSolid(Solid.make_cylinder(diameter / 2, self.dim.prongs.height))
-        hemisphere = SmartSolid(Solid.make_sphere(diameter / 2, angle1 = 0))
-        hemisphere.align_xy(cylinder).align_z(cylinder, Alignment.RR)
-        return SmartSolid(cylinder, hemisphere)
 
     # Creates slot in box for snap lock tip
     def create_lock_slot(self) -> SmartSolid:
@@ -337,27 +301,22 @@ class PowerAdapterBox(PowerAdapterBase):
 
         return SmartSolid(pencil.extrude(self.dim.lock.length_bottom + self.dim.box_taper_diff * 2))
 
-    # Clones element to create a row of sockets
-    def create_row(self, element: SmartSolid) -> SmartSolid:
-        return element.clone(self.dim.sockets_per_row, Vector(self.dim.socket_side + self.dim.socket_padding, 0))
 
 dimensions = PowerAdapterBoxDimensions()
 power_adapter_box = PowerAdapterBox(dimensions)
 power_adapter_lid = PowerAdapterLid(dimensions)
 
-def export_3mf(box: SmartSolid, prongs, lid: SmartSolid, snaps: SmartSolid, text: SmartSolid):
+def export_3mf(box: SmartSolid, lid: SmartSolid, snaps: SmartSolid, text: SmartSolid):
     export(lid)
     export(snaps)
     export(text)
 
     box.align_zxy(lid, Alignment.RR, -dimensions.lid_height - dimensions.box_wider_height)
-    prongs.colocate(box)
     export(box)
-    export(prongs)
 
     save_3mf("models/other/power_adapters/export.3mf", True)
 
-def export_stl(box: SmartSolid, prongs: SmartSolid, lid: SmartSolid, snaps: SmartSolid, text: SmartSolid):
+def export_stl(box: SmartSolid, lid: SmartSolid, snaps: SmartSolid, text: SmartSolid):
     clear()
 
     lid.orient((180, 0, 0))
@@ -365,7 +324,6 @@ def export_stl(box: SmartSolid, prongs: SmartSolid, lid: SmartSolid, snaps: Smar
     text.colocate(lid)
 
     export(box)
-    export(prongs)
     export(lid)
     export(snaps)
     export(text)
@@ -373,8 +331,8 @@ def export_stl(box: SmartSolid, prongs: SmartSolid, lid: SmartSolid, snaps: Smar
     save_stl("models/other/power_adapters/stl")
 
 
-box_solid, prongs_solid = power_adapter_box.create_box()
+box_solid = power_adapter_box.create_box()
 lid_solid, snaps_solid, text_solid = power_adapter_lid.create_lid()
 
-export_3mf(box_solid, prongs_solid, lid_solid, snaps_solid, text_solid)
-export_stl(box_solid, prongs_solid, lid_solid, snaps_solid, text_solid)
+export_3mf(box_solid, lid_solid, snaps_solid, text_solid)
+export_stl(box_solid, lid_solid, snaps_solid, text_solid)
